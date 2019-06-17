e0af179b5212c8c75e91e772450dba8bcd72b0a2
Сергей Василенко <vasilenko@puzzle-english.com>
2019-06-17 19:34:13 +0300

    Basic behavior

--- PomodoroTimer/ViewController.m
+++ PomodoroTimer/ViewController.m
@@ -10,14 +10,73 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UILabel *timerLabel;
+- (IBAction)toggleTimerAction:(UIButton *)sender;
+@property (strong, nonatomic) IBOutlet UIButton *toggleTimerButton;
+
+-(void) startTimer;
+-(void) updateTimerLabel;
+-(void) stopTimer;
+-(void) updateTimerWithSeconds: (int) s;
+
 @end
 
+
 @implementation ViewController
+
+BOOL timerStarted = NO;
+NSTimer *timer;
+int seconds;
+double timeMod = 1.0;
+int pomodoroMinutes = 25;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)toggleTimerAction:(UIButton *)sender {
+    
+    if(timerStarted)
+        [self stopTimer];
+    else
+        [self startTimer];
+}
+
+-(void) startTimer {
+    timerStarted = YES;
+    [_toggleTimerButton setTitle:@"Стоп" forState:UIControlStateNormal];
+    seconds = pomodoroMinutes * 60;
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 / timeMod target:self selector:@selector(updateTimerLabel) userInfo:nil repeats:YES];
+}
+
+-(void) stopTimer {
+    timerStarted = NO;
+    [timer invalidate];
+    timer = nil;
+    [_toggleTimerButton setTitle:@"Начать" forState:UIControlStateNormal];
+    
+    seconds = pomodoroMinutes * 60;
+    [self updateTimerWithSeconds: seconds];
+}
+
+-(void) updateTimerLabel
+{
+    if(seconds <= 0 )
+    {
+        [self stopTimer];
+        return;
+    }
+    seconds -= 1;
+    [self updateTimerWithSeconds: seconds];
+}
+
+-(void) updateTimerWithSeconds: (int) s {
+    NSString *minutesStr = [NSString stringWithFormat:@"%02i", s / 60];
+    NSString *secondsStr = [NSString stringWithFormat:@"%02i", s % 60];
+    NSString *timerString = [minutesStr stringByAppendingString: [@":" stringByAppendingString:secondsStr]];
+    [_timerLabel setText:timerString];
+}
 
 @end
